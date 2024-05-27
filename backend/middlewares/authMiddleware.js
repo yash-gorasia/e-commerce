57aeedb95aa765c4fa9ec337a55a5a3e01c914c1
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-05-27 18:57:09 +0530

    create middleware for authenticating the user and check if the user is admin or not

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import asyncHandler from './asyncHandler.js';
+
+const authenticate = asyncHandler(async (req, res, next) => {
+    let token;
+
+    // Read JWT from the 'JWT' cookie
+    token = req.cookie.JWT;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    } else {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+})
+
+// check if the user is an admin
+const authorizeAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401);
+        throw new Error('Not authorized as an admin');
+    }
+}
+
+export { authenticate, authorizeAdmin };
