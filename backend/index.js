8731f1ca78da1060fd2607bf0bdece4c7a3ad947
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-05-27 10:35:30 +0530

    created first route in index file

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ import cookieParser from 'cookie-parser';
 
 // Utiles
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+
 
 dotenv.config()
 const port = process.env.PORT || 5000;
@@ -18,8 +20,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running....');
-});
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
