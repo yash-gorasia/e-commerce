b80da52dad3e0eae66c041c29a67b5e46d16c644
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-05-27 18:00:58 +0530

    generateToken and store it in cookies  and code for creating user.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/userModel.js"; // Import the User model
 import asyncHandler from "../middlewares/asyncHandler.js"; // Import the asyncHandler middleware
+import bcrypt from "bcryptjs"; // Import bcrypt
+import generateToken from "../utils/createToken.js"; // Import the generateToken function
 
 const createUser = asyncHandler(async (req, res) => {
     const { username, email, password } = req.body; // Destructure the username, email, and password from req.body
@@ -14,6 +16,28 @@ const createUser = asyncHandler(async (req, res) => {
         res.status(400); // Set the status code to 400
         throw new Error("User already exists"); // Throw an error
     };
+
+    const salt = await bcrypt.genSalt(10); // Generate a salt, it will add random characters to the password
+    const hashedPassword = await bcrypt.hash(password, salt); // Hash the password
+
+    const newUser = await User.create({ username, email, password: hashedPassword }); // Create a new user
+
+    try {
+        newUser.save(); // Save the new user
+        generateToken(res, newUser._id); // Generate a token
+
+
+        res.status(201).json({ // Send a response
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            password: newUser.password,
+            isAdmin: newUser.isAdmin
+        });
+    } catch (error) {
+        res.status(400); 
+        throw new Error("Invalid user data"); 
+    }
 });
 
 export { createUser }; // Export the createUser function
---
+++ backend/utils/createToken.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (res,userId) => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, 
+        { expiresIn: '30d' });
+
+    // set JWT as an http-only cookie    
+    res.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+        secure: process.env.NODE_ENV !== 'development',
+        sameSite: 'strict',
+    });
+
+    return token;
+
+};
+
+export default generateToken;
