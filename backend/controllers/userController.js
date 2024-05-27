53be05bbec306d50162ecb37579f5427251adba7
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-05-27 18:41:23 +0530

    create route for logout and controller for logoutCurrentUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,4 +67,13 @@ const loginUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password"); // Throw an error
 });
 
-export { createUser, loginUser }; // Export the createUser, loginUser function
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+    res.cookie("token", "", { // Clear the cookie
+        httpOnly: true,
+        expires: new Date(0)
+    });
+
+    res.status(200).json({ message: "User logged out" }); // Send a response
+});
+
+export { createUser, loginUser, logoutCurrentUser }; // Export the createUser, loginUser function
