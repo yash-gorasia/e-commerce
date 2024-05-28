02de555fd849dbd6c632dc2c475f3e3deefc96cf
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-05-28 18:45:48 +0530

    initialized some variabels in login.jsx

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { useLoginMutation } from '../../redux/api/usersApiSlice'
+import { setCredentials } from '../../redux/features/auth/authSlice'
+import { toast } from 'react-toastify'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [login, { isLoading, isError, error }] = useLoginMutation()
+
+    const { userInfo } = useSelector((state) => state.auth)
+
+    const {search} = useLocation()
+    const sp = new URLSearchParams(search)
   return (
     <div>
-      
+
     </div>
   )
 }
