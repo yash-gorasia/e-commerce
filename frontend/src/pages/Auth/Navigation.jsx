7a43e0f52d6e6c3f99c54617e8755b7184f7d656
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-05-28 12:44:04 +0530

    build four features in nav bar still have to do work on it

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -1,4 +1,4 @@
-import { UseState } from 'react'
+import { useState } from 'react'
 import { AiOutlineHome, AiOutlineShopping, AiOutlineLogin, AiOutlineUserAdd, AiOutlineShoppingCart } from 'react-icons/ai'
 import { FaHeart } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
@@ -7,13 +7,69 @@ import "./Navigation.css"
 
 const Navigation = () => {
 
-    
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [showSidebar, setShowSidebar] = useState(false);
 
-  return (
-    <div>
-<h1>Navigation</h1>
-    </div>
-  )
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    return (
+        <>
+            <div
+                style={{ zIndex: 9999 }}
+                className={`${showSidebar ? "hidden" : "flex"
+                    } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-[#000] w-[4%] hover:w-[15%] h-[100vh]  fixed `}
+                id="navigation-container"
+            >
+                <div className="flex flex-col justify-center space-y-4">
+                    <Link
+                        to="/"
+                        className="flex items-center transition-transform transform hover:translate-x-2"
+                    >
+                        <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
+                        <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
+                    </Link>
+
+                    <Link
+                        to="/shop"
+                        className="flex items-center transition-transform transform hover:translate-x-2"
+                    >
+                        <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
+                        <span className="hidden nav-item-name mt-[3rem]">SHOP</span>{" "}
+                    </Link>
+
+                    <Link to="/cart" className="flex relative">
+                        <div className="flex items-center transition-transform transform hover:translate-x-2">
+                            <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26} />
+                            <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
+                        </div>
+
+                        {/* <div className="absolute top-9">
+                            {cartItems.length > 0 && (
+                                <span>
+                                    <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                                        {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                    </span>
+                                </span>
+                            )}
+                        </div> */}
+                    </Link>
+
+                    <Link to="/favorite" className="flex relative">
+                        <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+                            <FaHeart className="mt-[3rem] mr-2" size={20} />
+                            <span className="hidden nav-item-name mt-[3rem]">
+                                Favorites
+                            </span>{" "}
+                            {/* <FavoritesCount /> */}
+                        </div>
+                    </Link>
+                </div>
+                </div>
+            </>
+
+            )
 }
 
-export default Navigation
+            export default Navigation
