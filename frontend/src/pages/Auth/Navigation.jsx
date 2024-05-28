93afc6eb4fa3dc2af4f864ce319dce118c1b0d98
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2024-05-28 11:58:01 +0530

    create routing in main.jsx and made folders like pages, Auth and User and made Navigation file

---
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -0,0 +1,19 @@
+import { UseState } from 'react'
+import { AiOutlineHome, AiOutlineShopping, AiOutlineLogin, AiOutlineUserAdd, AiOutlineShoppingCart } from 'react-icons/ai'
+import { FaHeart } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import "./Navigation.css"
+
+const Navigation = () => {
+
+    
+
+  return (
+    <div>
+<h1>Navigation</h1>
+    </div>
+  )
+}
+
+export default Navigation
